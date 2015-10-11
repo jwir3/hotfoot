6a292defd3c279908d0c6426f2185eeeacf09e41
Scott Johnson <jaywir3@gmail.com>
2015-10-11 18:14:09 -0500

    Added basic github functionality.

--- program.js
+++ program.js
@@ -3,16 +3,52 @@ var path = require('path');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var conf = require('nconf-jsonminify');
+var GitHubAPI = require('github');
+var colors = require('colors');
 
 var timespan = require('./timespan.js');
+
+var github;
 
 main();
 
 function main() {
   initConfig();
+  initGithub();
 
-  var refreshTimeoutMs = timespan.getTimeSpanInMs(conf.get('refresh-timeout'));
+  var refreshTimeout = conf.get('refresh-timeout');
+  if (!refreshTimeout) {
+      return console.error("ERROR:".red + " You need to provide a 'refresh-timeout' specification in your configuration file");
+  }
+
+  var refreshTimeoutMs = timespan.getTimeSpanInMs(refreshTimeout);
   initializeRefreshLoop(refreshTimeoutMs);
+}
+
+function initGithub() {
+  github = new GitHubAPI({
+   // required
+   version: "3.0.0",
+   // optional
+   debug: true,
+   protocol: "https",
+   host: "api.github.com", // should be api.github.com for GitHub
+   pathPrefix: "", // for some GHEs; none for GitHub
+   timeout: 5000,
+   headers: {
+       "user-agent": "hotfoot/0.1.0" // GitHub is happy with a unique user agent
+   }
+   });
+
+   var userToken = conf.get('github-auth-token');
+   if (!userToken) {
+       return console.error("WARN:".yellow + " No github authentication token was specified in your config file. We won't be able to do much!");
+   }
+
+   github.authenticate({
+     type: 'token',
+     token: userToken
+   });
 }
 
 function initConfig() {
@@ -47,7 +83,20 @@ function verifyConfigDirCreated() {
 }
 
 function refresh() {
-  console.log("Refreshed at: " + new Date().toString());
+  // We want to perform the following on each refresh:
+  // 1. Download the list of github issues (and PRs, if enabled) for the given
+  //    repositories that have changed since the last refresh. These need to be
+  //    processed, one at a time.
+  // github.repos.getAll({}, function (err, data) {
+  //   if (err) {
+  //     return console.error("ERROR".red, err);
+  //   }
+  //
+  //   for (index in data) {
+  //     var object = data[index];
+  //     console.log(object.full_name);
+  //   }
+  // });
 }
 
 function initializeRefreshLoop(aTimeSpanMs) {
